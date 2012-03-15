1658d1900a0f710c1e34358dafb468c59cee789f
Ben Damer <bdamer@gmail.com>
2012-03-15 14:15:37 -0700

    Minor change to log view

--- protected/views/log/index.php
+++ protected/views/log/index.php
@@ -35,8 +35,8 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 </div><!-- search-form -->
 
 <?php $this->widget('zii.widgets.grid.CGridView', array(
-	'dataProvider'=>$model->search(),
 	'id'=>'log-grid',
+	'dataProvider'=>$model->search(),
 	'filter'=>$model,
 	'columns'=>array(
 		'id',
