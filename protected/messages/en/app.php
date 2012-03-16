66b47f41c9c3dd90ca73276935b404098285ba94
Ben Damer <bdamer@gmail.com>
2012-03-16 10:58:40 -0700

    Added string resources for English and German.

---
+++ protected/messages/en/app.php
@@ -0,0 +1,101 @@
+<?php
+return array(
+	// Page
+	'page.home'=>'Home',
+	'page.domains'=>'Domains',
+	'page.records'=>'Records',
+	'page.users'=>'Users',
+	'page.logs'=>'Logs',
+	'page.supermasters'=>'Supermasters',
+	'page.settings'=>'Settings',
+	'page.login'=>'Login',
+	'page.logout'=>'Logout',
+	'page.footer'=>'PDNSOps licensed under <a href="http://www.gnu.org/licenses/old-licenses/gpl-2.0.html">GNU GPL v2</a> &copy; ',
+	'page.requiredFields'=>'Fields with <span class="required">*</span> are required.',
+	'page.searchOperators'=>'You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
+or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.',
+	'page.create'=>'Create',
+	'page.update'=>'Update',
+	'page.copy'=>'Copy',
+	'page.add'=>'Add',
+	'page.save'=>'Save',
+	'page.search'=>'Search',
+	
+	// Domain
+	'domain.listDomains'=>'List Domains',
+	'domain.createDomain'=>'Create Domain',
+	'domain.createReverse'=>'Create Reverse Domain',
+	'domain.copyDomain'=>'Copy Domain',
+	'domain.updateDomain'=>'Update Domain',
+	'domain.id'=>'ID',
+	'domain.name'=>'Name',
+	'domain.ipAddress'=>'IP Address',
+	'domain.masterIP'=>'Master IP',
+	'domain.lastCheck'=>'Last Check',
+	'domain.type'=>'Type',
+	'domain.addDefaultNSRecords'=>'Add default NS records',
+	'domain.addDefaultMXRecord'=>'Add default MX record',
+	'domain.addDefaultCNAMERecords'=>'Add default CNAME records',
+	'domain.copyRecords'=>'Copy records?',
+	'domain.copyPermissions'=>'Copy permissions?',
+	'domain.onlyForSlave'=>'(Only for Slave domains)',
+	
+	// Log
+	'log.manage'=>'Manage Logs',
+	'log.id'=>'ID',
+	'log.user'=>'User',
+	'log.action'=>'Action',
+	'log.created'=>'Created',
+
+	// Login
+	'login.text'=>'Please fill out the following form with your login credentials:',
+	
+	// Record
+	'record.create'=>'Create Record',
+	'record.update'=>'Update Record',
+	'record.id'=>'ID',
+	'record.change_date'=>'Change Date',
+	'record.content'=>'Content',
+	'record.domain'=>'Domain',
+	'record.name'=>'Name',
+	'record.prio'=>'Priority',
+	'record.ttl'=>'TTL',	
+	'record.type'=>'Type',
+
+	// Settings
+	'setting.change'=>'Change global system settings.',
+	'setting.domainMasterIP'=>'Domain Master IP',
+	'setting.defaultTTL'=>'Default TTL',
+	'setting.soaRetry'=>'SOA Retry',
+	'setting.soaRefresh'=>'SOA Refresh',
+	'setting.soaExpire'=>'SOA Expire',
+	'setting.ns1'=>'Primary Nameserver',
+	'setting.ns2'=>'Secondary Nameserver',
+	'setting.ns3'=>'Tertiary Nameserver',
+	'setting.ns4'=>'Quaternary Nameserver',
+	'setting.ns5'=>'Quinary Nameserver',
+	'setting.ns6'=>'Senary Nameserver',
+	'setting.ns7'=>'Septenary Nameserver',
+	'setting.ns8'=>'Octonary Nameserver',
+	
+	// Supermaster
+	'supermaster.create'=>'Create Supermaster',
+	'supermaster.list'=>'List Supermasters',
+	'supermaster.update'=>'Update Supermaster',
+	'supermaster.ip'=>'IP',
+	'supermaster.nameserver'=>'Nameserver',
+	'supermaster.account'=>'Account',
+	'supermaster.ipInUse'=>'IP already in use.',
+	
+	// User
+	'user.id'=>'ID',
+	'user.username'=>'Username',
+	'user.email'=>'Email',
+	'user.password'=>'Password',
+	'user.passwordRepeat'=>'Password (repeat)',
+	'user.admin'=>'Admin?',
+	'user.usernameTaken'=>'Username already taken.',
+	'user.create'=>'Create User',
+	'user.list'=>'List Users',
+	'user.update'=>'Update User',
+);
