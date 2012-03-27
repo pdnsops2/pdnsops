8c430df457547f1b024b9fb641548a65d745623a
Ben Damer <bdamer@gmail.com>
2012-03-26 23:14:39 -0700

    Update schema to only create DNS tables if they do not exist.

--- protected/data/schema.mysql.sql
+++ protected/data/schema.mysql.sql
@@ -3,44 +3,37 @@ DROP TABLE IF EXISTS `domain_user`;
 DROP TABLE IF EXISTS `setting`;
 DROP TABLE IF EXISTS `log`;
 DROP TABLE IF EXISTS `user`;
-DROP TABLE IF EXISTS `domains`;
-DROP TABLE IF EXISTS `records`;
-DROP TABLE IF EXISTS `supermasters`;
 
 -- Create new tables
-create table domains (
- id		 INT auto_increment,
- name		 VARCHAR(255) NOT NULL,
- master		 VARCHAR(128) DEFAULT NULL,
- last_check	 INT DEFAULT NULL,
- type		 VARCHAR(6) NOT NULL,
- notified_serial INT DEFAULT NULL, 
- account         VARCHAR(40) DEFAULT NULL,
- primary key (id)
+CREATE TABLE IF NOT EXISTS `domains` (
+	`id` INT PRIMARY KEY AUTO_INCREMENT,
+	`name` VARCHAR(255) NOT NULL,
+	`master` VARCHAR(128) DEFAULT NULL,
+	`last_check` INT DEFAULT NULL,
+	`type` VARCHAR(6) NOT NULL,
+	`notified_serial` INT DEFAULT NULL, 
+	`account` VARCHAR(40) DEFAULT NULL,
+	UNIQUE INDEX `name_index` (`name`)
+)  Engine=InnoDB;
+
+CREATE TABLE IF NOT EXISTS `records` (
+	`id` INT PRIMARY KEY AUTO_INCREMENT,
+	`domain_id` INT DEFAULT NULL,
+	`name` VARCHAR(255) DEFAULT NULL,
+	`type` VARCHAR(10) DEFAULT NULL,
+	`content` VARCHAR(4096) DEFAULT NULL,
+	`ttl` INT DEFAULT NULL,
+	`prio` INT DEFAULT NULL,
+	`change_date` INT DEFAULT NULL,
+	INDEX `rec_name_index` (`name`),
+	INDEX `nametype_index` (`name`,`type`),
+	INDEX `domain_id` (`domain_id`)
 ) Engine=InnoDB;
 
-CREATE UNIQUE INDEX name_index ON domains(name);
-
-CREATE TABLE records (
-  id              INT auto_increment,
-  domain_id       INT DEFAULT NULL,
-  name            VARCHAR(255) DEFAULT NULL,
-  type            VARCHAR(10) DEFAULT NULL,
-  content         VARCHAR(4096) DEFAULT NULL,
-  ttl             INT DEFAULT NULL,
-  prio            INT DEFAULT NULL,
-  change_date     INT DEFAULT NULL,
-  primary key(id)
-) Engine=InnoDB;
-
-CREATE INDEX rec_name_index ON records(name);
-CREATE INDEX nametype_index ON records(name,type);
-CREATE INDEX domain_id ON records(domain_id);
-
-create table supermasters (
-  ip VARCHAR(25) NOT NULL, 
-  nameserver VARCHAR(255) NOT NULL, 
-  account VARCHAR(40) DEFAULT NULL
+CREATE TABLE IF NOT EXISTS supermasters (
+	`ip` VARCHAR(25) NOT NULL, 
+	`nameserver` VARCHAR(255) NOT NULL, 
+	`account` VARCHAR(40) DEFAULT NULL
 ) Engine=InnoDB;
 
 CREATE TABLE domain_user (
