33204f6e4bd5b79e39e7e1e78c61db18557573ea
Ben Damer <bdamer@gmail.com>
2012-03-26 23:33:42 -0700

    Removed test file. Added check for configuration to index.php

--- index.php
+++ index.php
@@ -4,10 +4,19 @@
 $yii=dirname(__FILE__).'/../yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
-// remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
-// specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+if (is_file($config))
+{
+	// remove the following lines when in production mode
+	//defined('YII_DEBUG') or define('YII_DEBUG',true);
+	// specify how many levels of call stack should be shown in each log message
+	defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
-require_once($yii);
-Yii::createWebApplication($config)->run();
+	require_once($yii);
+	Yii::createWebApplication($config)->run();
+}
+else
+{
+?>
+<p>Looks like you this is the first time you are running PDNSOps. Please run the <a href="./install/">installer</a> first.</p>
+<?
+}
